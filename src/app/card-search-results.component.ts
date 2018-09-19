b9dd1bd930fa770c374ae2ba30cd9fed87e27077
Christopher Chen <christopher.chen1995@gmail.com>
2018-09-19 08:09:16 -0700

    Autoformat

--- src/app/card-search-results.component.ts
+++ src/app/card-search-results.component.ts
@@ -1,110 +1,100 @@
 import { Component, Input, OnInit, OnDestroy } from "@angular/core";
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/combineLatest';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Location } from '@angular/common';
-import { Card } from './card'
-import { CardSearchService, SearchResult } from './card-search.service';
+import { combineLatest } from "rxjs";
 
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Location } from "@angular/common";
+import { Card } from "./card";
+import { CardSearchService, SearchResult } from "./card-search.service";
 
 @Component({
-	selector: 'card-search-results',
-	templateUrl: './card-search-results.component.html',
-	styleUrls: ['./card-search-results.component.css',],
-
+  selector: "card-search-results",
+  templateUrl: "./card-search-results.component.html",
+  styleUrls: ["./card-search-results.component.css"]
 })
-
 export class CardSearchResultsComponent implements OnInit, OnDestroy {
-	card: Card;
-	cards: Card[];
-	name: string = '';
-	page: number = 1;
-	colorIdentity: string = '';
-	json = JSON;
-	isLoading: boolean = false;
-	subscriptions = [];
+  card: Card;
+  cards: Card[];
+  name: string = "";
+  page: number = 1;
+  colorIdentity: string = "";
+  json = JSON;
+  isLoading: boolean = false;
+  subscriptions = [];
 
-	constructor(
-		private cardSearchService: CardSearchService,
+  constructor(
+    private cardSearchService: CardSearchService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router
+  ) {
+    console.log("CardSearchResultsComponent instantiated");
 
-		private route: ActivatedRoute,
-		private location: Location,
-		private router: Router,
+    let subMaker = combineLatest(
+      this.route.params,
+      this.route.queryParams,
+      (params, qparams) => ({ params, qparams })
+    );
 
-	) {
-		console.log('CardSearchResultsComponent instantiated');
+    let sub = subMaker.subscribe((allParams: Params) => {
+      this.isLoading = true;
 
-		let subMaker = Observable.combineLatest(this.route.params, this.route.queryParams, (params, qparams) => ({ params, qparams }));
+      if (allParams.qparams["ci"] !== undefined) {
+        this.colorIdentity = allParams.qparams["ci"];
+        console.log("ci=", this.colorIdentity);
+      }
 
-		let sub = subMaker.subscribe((allParams: Params) => {
-			this.isLoading = true;
+      let name = "";
+      if (allParams.params["name"] !== undefined) {
+        this.name = allParams.params["name"];
+      }
+      if (allParams.params["page"] !== undefined) {
+        this.page = +allParams.params["page"];
+      }
 
-			if (allParams.qparams['ci'] !== undefined) {
-				this.colorIdentity = allParams.qparams['ci'];
-				console.log("ci=", this.colorIdentity);
-			}
+      this.query();
+    });
 
-			let name = '';
-			if (allParams.params['name'] !== undefined) {
-				this.name = allParams.params['name'];
+    this.subscriptions.push(sub);
+  }
 
-			}
-			if (allParams.params['page'] !== undefined) {
-				this.page = +allParams.params['page'];
-			}
+  ngOnInit(): void {}
 
+  query(): void {
+    this.cardSearchService
+      .searchSimilar(this.name, this.page, this.colorIdentity)
+      .toPromise()
+      .then(obj => {
+        this.cards = obj.results;
+        this.card = obj.card;
+        this.isLoading = false;
+      });
+  }
+  ngOnDestroy(): void {
+    for (let sub of this.subscriptions) {
+      sub.unsubscribe();
+    }
+  }
 
-			this.query();
+  previousPage(): void {
+    this.page -= 1;
+    this.go();
+  }
 
+  go(): void {
+    this.router.navigate([
+      "/similar",
+      this.name,
+      this.page,
+      { ci: this.colorIdentity }
+    ]);
+  }
 
-		});
+  nextPage(): void {
+    this.page += 1;
+    this.go();
+  }
 
-		this.subscriptions.push(sub);
-
-
-	}
-
-	ngOnInit(): void {
-
-
-
-	}
-
-	query(): void {
-
-		this.cardSearchService.searchSimilar(this.name, this.page, this.colorIdentity)
-			.toPromise()
-			.then(obj => {
-				this.cards = obj.results;
-				this.card = obj.card;
-				this.isLoading = false;
-			});
-	}
-	ngOnDestroy(): void {
-		for (let sub of this.subscriptions) {
-			sub.unsubscribe();
-		}
-	}
-
-	previousPage(): void {
-		this.page -= 1;
-		this.go();
-	}
-
-	go(): void {
-		this.router.navigate(['/similar', this.name, this.page, { 'ci': this.colorIdentity }]);
-
-	}
-
-
-	nextPage(): void {
-		this.page += 1;
-		this.go();
-	}
-
-
-
-	goBack(): void {
-		this.router.navigate(['/search']);
-	}
+  goBack(): void {
+    this.router.navigate(["/search"]);
+  }
 }
